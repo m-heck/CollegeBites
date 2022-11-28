f596b0c22b52210e4e445c8ccdbed5043cbaa743
Maren Heck <marenjheck@gmail.com>
2022-11-28 13:11:54 -0500

    outlined the logic for merge sort

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,10 @@
+// ============================ MAIN ============================
+
 console.log(data);
 console.log(filterData("Nashville"));
+mergeSort(data, "Rating");
+
+// ============================ WEBSITE FUNCTIONALITY ============================
 
 // FILTER DATA
 // Input: a string of the name of the city you would like to filter for
@@ -14,13 +19,29 @@ function filterData(searchCity) {
     return result;
 }
 
+// ============================ ALGORITHM 1 ============================
+
+// Credit: Logic behind merge sort from class slides "Sorting" slides #89, 90
 // MERGE SORT
 // Input: an array to be sorted, the method by which to sort the items (rankBy)
 //      rankBy can be one of two strings, "Rating" or "Popularity"
 // Output: a sorted array of objects
 function mergeSort(array, rankBy) {
+    mergeSortHelper(array, 0, array.length - 1, rankBy);
+}
+
+// Credit: Logic behind merge sort from class slides "Sorting" slides #89, 90
+// MERGE SORT HELPER
+function mergeSortHelper(array, left, right, rankBy) {
+    console.log("Testing");
+}
+
+// MERGE
+function merge(array, left, mid, right) {
 
 }
+
+// ============================ ALGORITHM 2 ============================
 
 // QUICK SORT
 // Input: an array to be sorted, the method by which to sort the items (rankBy)
@@ -30,5 +51,3 @@ function mergeSort(array, rankBy) {
 function quickSort(array, rankBy) {
 
 }
-
-// HELPER FUNCTION
