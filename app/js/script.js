a44ec301241109f433e105c98699033c6c3cfca1
Maren Heck <marenjheck@gmail.com>
2022-11-28 19:31:08 -0500

    comments that describe helpful functions

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d01b19bef01ac36f526ff0c560f0b4323ab32d87670e3bf6fc7424b977b644c
-size 6716
+oid sha256:8a9686a312c69fd9290610391d3789a296ccf32433a3f3b68524a856cff7e9a0
+size 7097
