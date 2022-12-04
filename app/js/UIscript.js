f6569ff2f75e830e0a4165c6f77fdf280d77b865
Maren Heck <marenjheck@gmail.com>
2022-12-04 18:17:40 -0500

    changed popularity to review count

--- app/js/UIscript.js
+++ app/js/UIscript.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5c3cbcc406631492f028a814389382e8e3abdaea474d2d72704c497ebe6ec15
-size 4917
+oid sha256:17e28a952d69fab3ca177511218584f2c37117a1c919e0e32f46db39e97c4efe
+size 4919
