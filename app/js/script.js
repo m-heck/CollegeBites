3535289982fb9676ccf7bdfb0d5953ca556c89a4
Maren Heck <marenjheck@gmail.com>
2022-12-04 19:07:05 -0500

    cleaning up script.js more

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:931bb6945823895fb8db20f87af939380ebd4e47dcb2447024694deafcc782ab
-size 6719
+oid sha256:2db315d573cd34643800e619a0790319dffcff71dd05ad58106e96614bf77234
+size 5955
